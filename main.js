9334d84b844d2c14e42649ce0c684bf33ff9912f
Niravro11 <75479734+Niravro11@users.noreply.github.com>
2021-10-25 18:13:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1 = "";
 song2= "";
+leftwristX= 0;
+leftwristY= 0;
+rightwristX= 0;
+rightwristY= 0;
+
 
 function preload() {
     song = loadSound("music.mp3");
@@ -13,8 +18,28 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotposes);
 }
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+ }
+ 
 
 function draw() {
 	image(video, 0, 0, 600, 500);
 }
+function gotposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftwristX= results[0].pose.leftWrist.x;
+        leftwristY= results[0].pose.leftWrist.y;
+        console.log("leftwristX = " + leftwristX + "leftwristY = " + leftwristY);
+       
+        rightwristX= results[0].pose.rightWrist.x;
+        rightwristY= results[0].pose.rightWrist.y;
+        console.log("rightwristX = " + rightwristX + "rightwristY = " + rightwristY);
+    }
+ }
+ 
+ 
